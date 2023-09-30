b67882e9e53c68ac3fcebeb72079a3717ff89621
Your Name <you@example.com>
2023-09-30 17:52:49 +0530

    test 1

--- src/java_problems_ganesh/welcome.java
+++ src/java_problems_ganesh/welcome.java
@@ -3,7 +3,6 @@ package java_problems_ganesh;
 public class welcome {
 public static void main(String[] args) {
 	System.out.println("welcome to git");
-	System.out.println("welcome to git");
 	
 }
 }
