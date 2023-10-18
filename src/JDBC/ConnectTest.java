bb989fc64cabd255c45ac8756d5922ef8a3cb002
Your Name <you@example.com>
2023-10-18 20:25:53 +0530

    date 18/10

---
+++ src/JDBC/ConnectTest.java
@@ -0,0 +1,63 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class ConnectTest {
+	public static void main(String[] args) throws Exception{
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter name");
+		String name = input.nextLine();
+		System.out.println("enter email");
+		String email = input.nextLine();
+		System.out.println("enter password");
+		String password = input.nextLine();
+		System.out.println("enter gender");
+		String gender = input.nextLine();
+		System.out.println("enter city");
+		String city = input.nextLine();
+		
+		//using variable names
+		/*String name = "samantha";
+		String email = "sam143@gmail.com";
+		String password = "143surya";
+		String gender = "female";
+		String city = "hyderabad";*/
+
+		//	step 1 to establish connection 
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		// step 2 establishing connection with server
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/node", "root", "pass123"); 
+		
+		
+		// step 3 updating the values
+		//type 1 to enter values
+		//PreparedStatement ps=con.prepareStatement
+		//		("insert into register values('surya','surya@gmail.com','123','male','proddatur')");
+		//type 2 to entering values using variable
+		//PreparedStatement ps=con.prepareStatement
+			//	("insert into register values('"+name+"','"+email+"','"+password+"','"+gender+"','"+city+"')");
+		//type 3 positional parameters
+		PreparedStatement ps=con.prepareStatement
+				("insert into register values(?,?,?,?,?)");
+		ps.setString(1, name);
+		ps.setString(2, email);
+		ps.setString(3, password);
+		ps.setString(4, gender);
+		ps.setString(5, city);
+		
+		//executing the statement using update
+		int i =ps.executeUpdate();
+		//printing success msg as above statement returns int as return
+		if (i>0) {
+			System.out.println("saved to table node");
+		}
+		else System.out.println("error not sav");
+		//need to close connection and input stream after completion
+		con.close();
+		input.close();
+	}
+}
--- src/java_problems_surya/Roman.java
+++ src/java_problems_surya/Roman.java
@@ -1,4 +1,7 @@
 package java_problems_surya;
+
+import java.util.*;
+
 /**
  * I             1
 V             5
@@ -10,40 +13,95 @@ M             1000
  */
 public class Roman {
 public static void main(String[] args) {
-	int i = 1;
-	int v = 5;
-	int x = 10;
-	int l = 50;
-	int c = 100;
-	int d = 500;
-	int m = 1000;
-	
-	String input = "IX";
+	HashMap<Character, Integer> num = new HashMap<>();
+	num.put('i', 1);	
+	num.put('v', 5);	
+	num.put('x', 10);	
+	num.put('l', 50);	
+	num.put('c', 100);	
+	num.put('d', 500);	
+	num.put('m', 1000);	
 	int value = 0;
-	char[] k = input.toCharArray();
-	for(int j =0;j<k.length;j++) {
-		if(k[j]=='I') {
-			value+=i;
+	String input = "IX";
+	char[] k = input.toLowerCase().toCharArray();
+//	for (char c : k) {
+//		System.out.println(c);
+//	}
+	for (int i = 0; i < k.length; i++) {
+		if() {
+			
 		}
-		if(k[j]=='L') {
-			value+=l;
-		}
-		if(k[j]=='V'||k[j-1]=='I') {
-			value+=v-i-1;
-		}
-		if(k[j]=='X') {
-			value+=x;
-		}
-		if(k[j]=='C') {
-			value+=c;
-		}
-		if(k[j]=='D') {
-			value+=d;
-		}
-		if(k[j]=='M') {
-			value+=d;
+		else {
+		value+=num.get(k[i]);
 		}
 	}
 	System.out.println(value);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	int i = 1;
+//	int v = 5;
+//	int x = 10;
+//	int l = 50;
+//	int c = 100;
+//	int d = 500;
+//	int m = 1000;
+	
+//	String input = "IX";
+//	int value = 0;
+//	char[] k = input.toCharArray();
+//	for(int j =0;j<k.length;j++) {
+//		if(k[j]=='I') {
+//			value+=i;
+//		}
+//		if(k[j]=='L') {
+//			value+=l;
+//		}
+//		if(k[j]=='V'||k[j-1]=='I') {
+//			value+=v-i-1;
+//		}
+//		if(k[j]=='X') {
+//			value+=x;
+//		}
+//		if(k[j]=='C') {
+//			value+=c;
+//		}
+//		if(k[j]=='D') {
+//			value+=d;
+//		}
+//		if(k[j]=='M') {
+//			value+=d;
+//		}
+//	}
+//	System.out.println(value);
 }
 }
