959a2fde77d34e0a19b3f16e8576f96be306ebee
Your Name <you@example.com>
2023-11-01 08:35:57 +0530

    date 1/11

--- src/java_problems_surya/Coding_Problems.java
+++ src/java_problems_surya/Coding_Problems.java
@@ -119,21 +119,86 @@ public class Coding_Problems {
 	 */
 
 	public int operationchoices(int c, int a, int b) {
-		
+
 		if(c==1) return a+b;
 		else if(c==4) return a/b;
 		else if(c==2) return a-b;
 		else if(c==3) return a*b;
 		else return 0;
-	
+
 	}
+
+	/**
+	 * Perform the function Int calculate(int m, int n). This function needs two 	positive integers. Calculate the sum of numbers between these two numbers that are 	divisible by 3 and 5.
+	Assumption
+	m > n > = 0
+	Example
+	Input:
+	m: 12
+	n: 50
+	Output:
+	90
+
+	Explanation
+	The numbers between 12 and 50 that are divisible by 3 and 5 are 15, 30, and 45. 	The sum of these numbers is 90.
+
+	Sample input:
+	m: 100
+	n: 160
+	Sample output:
+	405
+	 * 
+	 */
+	public int calculate(int m, int n) {
+		int sum=0;
+		for(int i =m ; i<=n;i++) {
+			if(i%3==0&&i%5==0) {
+				sum+=i;
+			}
+		}
+		return sum;
+	}
+
+	/**auto biographical number
+	 * 
+-	 */
+	public int autoCount(String n) {
+		int zero=0,one=0,result=0;
+		if (n.length()>10||n=="")	return 0;
+		else {
+			//to count the one's and zero's
+			for (int i = 0; i < n.length(); i++) {
+				if (n.charAt(i)=='0') zero=zero+1;
+				if (n.charAt(i)=='1') one=one+1;
+			}}
+		// to check whether  the number is autobiographical number and distinct numbers in it
+		if (Character.getNumericValue(n.charAt(0))==zero && Character.getNumericValue(n.charAt(1))==one) {
+			for (int i = 0; i < n.length(); i++) 
+				if (n.charAt(i)!='0') result++; return result;
+		} else return 0;
+	}
+
+	
+	public String isAnagram(String s1,String s2) {
+		
+		int lenght = s1.length(), check=0;
+		for (int i = 0; i < s1.length(); i++) 
+			for (int j = 0; j < s2.length(); j++) {
+				if (s1.charAt(i)==s2.charAt(j)) check++;
+			}
+		if(check==lenght) return "Anagram";
+		else return "Not Anagram";	}
+	
+	
 	public static void main(String[] args) {
 		Coding_Problems obj = new Coding_Problems();
 		//		int result = obj.differenceofSum(10, 3);
 		//		System.out.println("this is a method result: "+result);
 		//		int arr[]= {3,2,1,7,5,4};
 		//		obj.LargeSmallSum(arr);
-		System.out.println(obj.operationchoices(1, 16, 12));
-		
+		//		System.out.println(obj.operationchoices(1, 16, 12));	
+		//		System.out.println(obj.calculate(12, 50));
+				System.out.println(obj.isAnagram("race", "caare"));
 	}
+	
 }
---
+++ src/java_problems_surya/Java_100Problems.java
@@ -0,0 +1,121 @@
+package java_problems_surya;
+
+
+public class Java_100Problems {
+/**
+ * 1.Simple Java Program
+ */
+//	public static void main (String[] args) {
+//	System.out.println("Hi  Aarthi");	
+//	}
+	
+	/**
+	 * 2.Print Integer in java
+	 */
+//	public static void main (String[] args) {
+//		int a =2;
+//		System.out.println(a);
+//		}
+	/**
+	 * 3.Command Line Argument in java
+	 */
+//	public static void main (String[] args) {
+//		for (String string : args) {
+//		System.out.println(string);
+//		}
+//		}
+	/**
+	 * 4.How to get Using input using Scanner
+			Program in java
+	 */
+//	public static void main (String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("what is your name");
+//		String name=sc.next();
+//		System.out.println(name);
+//		sc.close();
+//		}
+	/**
+	 * 5.How to convert Fahrenheit to Celsius
+		Program in java
+		formula:°C = (°F - 32) × 5/9
+	 */
+//	public static void main (String[] args) {
+//		float f = 100;
+//		float cal = (f-32) * 5/9;
+//		System.out.println(cal+"°C");
+//		}
+//	
+	/**
+	 * 6.How to swap 2 no using 3rd variable Program in java
+	 * x = a 
+	 * y = b 
+	 *  x=b
+	 *  y=a
+	 */
+	
+//	public static void main (String[] args) {
+//		int x = 10;
+//		int y =	20;
+//		int z = x;
+//		x=y;
+//		y=z;
+//		System.out.println(x+" "+y);
+//		}
+	
+	/**
+	 * 7.How to swap 2 no without using 3rd variable
+		Program in java
+	 */
+	
+//	public static void main (String[] args) {
+//		int m=	20;
+//		int n = 10;
+//		m=m+n;//30
+//		n=m-n;//20
+//		m=m-n;//10
+//		System.out.println(m+" "+n);
+//		}
+	
+	
+	/**
+	 * 8.How to add two number Program in java
+	 */
+//	public static void main (String[] args) {
+//		int m=	20;
+//		int n = 10;
+//		System.out.println(m+n);
+//		}
+	
+	
+	/**
+	 * 9.Find Largest no in java Program
+	 */
+//	public static void main (String[] args) {
+//		int a =100;
+//		int b=60;
+//		int c=30;
+//		if (a>b&&a>c) {
+//			System.out.println(a);
+//		}
+//		if (b>a&&b>c) {
+//			System.out.println(b);
+//		}
+//		if (c>b&&c>a) {
+//			System.out.println(c);
+//		}
+//		}
+	
+	/**
+	 * 10. If Else clause in java
+	 */
+	public static void main (String[] args) {
+		int marks = 15;
+		if (marks>35) {
+			System.out.println("pass");
+		}
+		else System.out.println("fail");
+		}
+	
+}
+
